8030c9a9cb54acea800f74ed0d49691b8f316f6e
Geremias Santos <Geremias99999@gmail.com>
2022-06-15 11:39:30 -0300

    Exercise Class C#

---
+++ OlderPeople/Class/People.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OlderPeople.Class
+{
+    public class People
+    {
+        public string Name { get; set; }
+        public int Years { get; set; }
+
+        public People()
+        {
+        }
+        public People(string name, int years)
+        {
+            Name = name;
+            Years = years;
+        }
+    }
+}
---
+++ OlderPeople/Program.cs
@@ -0,0 +1,19 @@
+﻿using OlderPeople.Class;
+using System;
+
+namespace OlderPeople
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            People p = new People();
+            Console.Write("Insert your name: ");
+            p.Name = Console.ReadLine();
+            Console.Write("Insert you age: ");
+            p.Years = int.Parse(Console.ReadLine());
+            Console.Write("Your name is " + p.Name + " and " + "your age is " + p.Years);
+         
+        }
+    }
+}
